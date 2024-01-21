560b76c8045bbf207b23837ae3952fb12a4438c8
Eduard Luca <edu2004eu@users.noreply.github.com>
2024-01-21 23:29:37 +0200

    changed urls.py to django 4.x format

--- django_select2/urls.py
+++ django_select2/urls.py
@@ -6,11 +6,10 @@ Add `django_select` to your ``urlconf`` **if** you use any 'Model' fields::
     url(r'^select2/', include('django_select2.urls')),
 
 """
-from django.conf.urls import url
+from django.urls import path
 
 from .views import AutoResponseView
 
 urlpatterns = [
-    url(r"^fields/auto.json$",
-        AutoResponseView.as_view(), name="django_select2-json"),
+    path("fields/auto.json", AutoResponseView.as_view(), name="django_select2-json"),
 ]
